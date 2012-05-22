e3f60864f903b6eb229e14e5cade7efb8cbde4cd
devinrader <devin@twilio.com>
2012-05-22 13:25:17 -0400

    removing initializer

--- tally.vu.web/App_Start/DataAccess.cs
+++ tally.vu.web/App_Start/DataAccess.cs
@@ -17,14 +17,14 @@ namespace tallyvu.Website.App_Start
 
         public static void InitializeDatabase()
         {
-            IDatabaseInitializer<DataContext> initializer;
+            //IDatabaseInitializer<DataContext> initializer;
 
-            if (IsAppHarbor)
-                initializer = new CreateDatabaseIfNotExists<DataContext>();// DontDropDbJustCreateTablesIfModelChanged<DataContext>();
-            else
-                initializer = new DropCreateDatabaseAlways<DataContext>();
+            //if (IsAppHarbor)
+            //    initializer = new CreateDatabaseIfNotExists<DataContext>();// DontDropDbJustCreateTablesIfModelChanged<DataContext>();
+            //else
+            //    initializer = new DropCreateDatabaseAlways<DataContext>();
 
-            Database.SetInitializer(new DataContext.Initializer(initializer));
+            //Database.SetInitializer(new DataContext.Initializer(initializer));
 
             using (var context = new DataContext())
             {
